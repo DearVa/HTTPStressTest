0f6c4825b24bf9bf964fe0da3b8cac860c344e2a
DearVa <55902546+DearVa@users.noreply.github.com>
2020-02-06 18:56:44 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace HTTPStressTest {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_code = new System.Windows.Forms.Label();
+            this.label_failed = new System.Windows.Forms.Label();
+            this.label_success = new System.Windows.Forms.Label();
+            this.label_tested = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(581, 100);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "网址：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "次数：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(20, 166);
+            this.textBox2.MaxLength = 10;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "0";
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label_code);
+            this.groupBox1.Controls.Add(this.label_failed);
+            this.groupBox1.Controls.Add(this.label_success);
+            this.groupBox1.Controls.Add(this.label_tested);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 205);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(589, 80);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "状态";
+            // 
+            // label_code
+            // 
+            this.label_code.AutoSize = true;
+            this.label_code.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label_code.Location = new System.Drawing.Point(353, 21);
+            this.label_code.Name = "label_code";
+            this.label_code.Size = new System.Drawing.Size(37, 15);
+            this.label_code.TabIndex = 7;
+            this.label_code.Text = "未知";
+            // 
+            // label_failed
+            // 
+            this.label_failed.AutoSize = true;
+            this.label_failed.Location = new System.Drawing.Point(353, 52);
+            this.label_failed.Name = "label_failed";
+            this.label_failed.Size = new System.Drawing.Size(15, 15);
+            this.label_failed.TabIndex = 6;
+            this.label_failed.Text = "0";
+            // 
+            // label_success
+            // 
+            this.label_success.AutoSize = true;
+            this.label_success.Location = new System.Drawing.Point(93, 52);
+            this.label_success.Name = "label_success";
+            this.label_success.Size = new System.Drawing.Size(15, 15);
+            this.label_success.TabIndex = 5;
+            this.label_success.Text = "0";
+            // 
+            // label_tested
+            // 
+            this.label_tested.AutoSize = true;
+            this.label_tested.Location = new System.Drawing.Point(93, 21);
+            this.label_tested.Name = "label_tested";
+            this.label_tested.Size = new System.Drawing.Size(15, 15);
+            this.label_tested.TabIndex = 4;
+            this.label_tested.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(265, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "状态码：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(265, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 15);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "失败次数：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "成功次数：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "已测次数：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 53);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(160, 146);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 15);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "请求方式：";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(163, 172);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 19);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "GET";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(221, 172);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 19);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.Text = "POST";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(317, 146);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 15);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "超时：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(320, 166);
+            this.textBox3.MaxLength = 10;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(99, 25);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "500";
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(622, 294);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "网站压力测试";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_code;
+        private System.Windows.Forms.Label label_failed;
+        private System.Windows.Forms.Label label_success;
+        private System.Windows.Forms.Label label_tested;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Net;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace HTTPStressTest {
+    public partial class Form1 : Form {
+
+        private bool testing = false;
+        private Thread thread;
+        private int t, tested, success, failed, timeout = 500;
+        private Uri uri;
+        private string method = "GET";
+        private SynchronizationContext syncContext_tested = null;
+        private SynchronizationContext syncContext_success = null;
+        private SynchronizationContext syncContext_failed = null;
+        private SynchronizationContext syncContext_code = null;
+        private SynchronizationContext syncContext_button = null;
+
+        public Form1() {
+            InitializeComponent();
+            syncContext_tested = SynchronizationContext.Current;
+            syncContext_success = SynchronizationContext.Current;
+            syncContext_failed = SynchronizationContext.Current;
+            syncContext_code = SynchronizationContext.Current;
+            syncContext_button = SynchronizationContext.Current;
+        }
+
+        private void PostTested(object text) {
+            label_tested.Text = text.ToString();
+        }
+
+        private void PostSuccess(object text) {
+            label_success.Text = text.ToString();
+        }
+
+        private void PostFailed(object text) {
+            label_failed.Text = text.ToString();
+        }
+
+        private void PostCode(object text) {
+            label_code.Text = text.ToString();
+            if ((int)text >= 400) {
+                label_code.ForeColor = Color.Red;
+            } else if ((int)text >= 300) {
+                label_code.ForeColor = Color.Yellow;
+            } else {
+                label_code.ForeColor = Color.Black;
+            }
+        }
+
+        private void PostButton(object text) {
+            button1.Text = text.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (testing) {
+                button1.Text = "开始";
+                if (thread != null) {
+                    thread.Abort();
+                }
+            } else {
+                try {
+                    if (textBox1.Text.Substring(0, 7) != "http://" || textBox1.Text.Substring(0, 8) != "https://") {
+                        uri = new Uri("http://" + textBox1.Text);
+                    }
+                } catch {
+                    MessageBox.Show("请输入正确的网址", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (textBox3.Text == "0") {
+                    MessageBox.Show("超时不能为0", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                timeout = int.Parse(textBox3.Text);
+                t = int.Parse(textBox2.Text);
+                tested = success = failed = 0;
+                thread = new Thread(new ThreadStart(StartRequest));
+                thread.Start();
+                button1.Text = "停止";
+            }
+            testing = !testing;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            if (thread != null) {
+                thread.Abort();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) {
+            if (radioButton1.Checked) {
+                method = "GET";
+            } else {
+                method = "POST";
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e) {
+            if (!(Char.IsNumber(e.KeyChar) || e.KeyChar == '\b')) {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e) {
+            if (!(Char.IsNumber(e.KeyChar) || e.KeyChar == '\b')) {
+                e.Handled = true;
+            }
+        }
+
+        private void StartRequest() {
+            if (t == 0) {
+                while (true) {
+                    Request();
+                }
+            } else {
+                for(int i = 0; i < t; i++) {
+                    Request();
+                }
+                syncContext_button.Post(PostButton, "开始");
+            }
+            
+        }
+        private void Request() {
+            int httpStatusCode;
+            try {
+                tested++;
+                WebRequest myReq = WebRequest.Create(uri);
+                myReq.Timeout = timeout;
+                myReq.Method = method;
+                var rsp = myReq.GetResponse() as HttpWebResponse;
+                httpStatusCode = (int)rsp.StatusCode;
+                rsp.Close();
+            } catch {
+                httpStatusCode = 404;
+            }
+            if (httpStatusCode >= 400) {
+                failed++;
+            } else {
+                success++;
+            }
+            syncContext_code.Post(PostCode, httpStatusCode);
+            syncContext_failed.Post(PostFailed, failed);
+            syncContext_tested.Post(PostTested, tested);
+            syncContext_success.Post(PostSuccess, success);
+        }
+    }
+}
